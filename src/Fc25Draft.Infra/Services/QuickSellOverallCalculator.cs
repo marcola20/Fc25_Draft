b51ad265906f92f665ee8dd219178d0cadd99563
Marcola <sbmarcos777@gmail.com>
2026-01-10 18:49:25 -0300

    feat: adicionado aumento de over para jogadores 87, 88 e 89

--- src/Fc25Draft.Infra/Services/QuickSellOverallCalculator.cs
+++ src/Fc25Draft.Infra/Services/QuickSellOverallCalculator.cs
@@ -18,23 +18,29 @@ internal static class QuickSellOverallCalculator
         if (randomInclusive is null)
             throw new ArgumentNullException(nameof(randomInclusive));
 
-        if (currentOverall >= 87)
+        if (currentOverall  >= 90)
             return 0;
 
-        if (currentOverall is >= 85 and <= 86)
+        if (currentOverall is >= 89 and < 90)
+            return randomInclusive(1, 1);
+
+        if (currentOverall is >= 87 and <= 88)
             return randomInclusive(1, 2);
 
-        if (currentOverall is >= 83 and <= 84)
+        if (currentOverall is >= 85 and <= 86)
             return randomInclusive(2, 3);
 
-        if (currentOverall is >= 81 and <= 82)
+        if (currentOverall is >= 83 and <= 84)
             return randomInclusive(3, 4);
 
-        if (currentOverall is 80)
+        if (currentOverall is >= 81 and <= 82)
             return randomInclusive(4, 5);
 
+        if (currentOverall is 80)
+            return randomInclusive(5, 6);
+
         if (currentOverall is >= 77 and <= 79)
-            return randomInclusive(5, 7);
+            return randomInclusive(7, 8);
 
         return 0;
     }
