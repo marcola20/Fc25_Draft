0f6eb7c6cb716cf2745f247e9c18f573faa15931
Marcola <sbmarcos777@gmail.com>
2026-09-15 16:09:17 -0300

    fix: over do draft

--- src/Fc25Draft.Core/Utilities/DraftWishlistRules.cs
+++ src/Fc25Draft.Core/Utilities/DraftWishlistRules.cs
@@ -3,7 +3,7 @@ namespace Fc25Draft.Core.Utilities;
 /// <summary>Regras da lista de pré-draft: os 8 jogadores mais interessantes de cada time.</summary>
 public static class DraftWishlistRules
 {
-    public const int MaxJogadores = 8;
+    public const int MaxJogadores = 10;
     public const int OverallMinimo = 0;
-    public const int OverallMaximo = 86;
+    public const int OverallMaximo = 87;
 }
